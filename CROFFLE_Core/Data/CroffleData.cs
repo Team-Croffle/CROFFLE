90be082d7ab3ca7444ba5b557ff354dedfeec91b
Ahn GyuTae <xpsj20@gmail.com>
2024-11-07 22:24:18 +0900

    ::Project - CROFFLE_Core::
    By BlueNyang <xpsj20@gmail.com>

---
+++ CROFFLE_Core/Data/CroffleData.cs
@@ -0,0 +1,14 @@
+﻿using DataManager.SQLiteDBMS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CROFFLE_Core.Data
+{
+    internal class CroffleData
+    {
+
+    }
+}
