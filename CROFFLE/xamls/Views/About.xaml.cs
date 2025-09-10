41829fc04ad6a94d3bc7e9a819d7c20e3fa121a2
BlueNyang <xpsj20@gmail.com>
2025-09-10 22:14:42 +0900

    chore: delete all c# proj and setting for electron framwork

--- CROFFLE/xamls/Views/About.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace CROFFLE.xamls.Views;
-
-public partial class About : ContentPage
-{
-	public About()
-	{
-		InitializeComponent();
-	}
-}
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  base: "./",
+  root: "src/renderer",
+  build: {
+    outDir: "../../dist/renderer",
+  },
+});
