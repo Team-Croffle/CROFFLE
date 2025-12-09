40153660a727c20edc45799d6a51d9a058ad42af
BlueNyang <dksrbxo@bluenyang.kr>
2025-12-09 10:13:57 +0900

    feat: add PluginInfo, Settings Entities

---
+++ app/main/src/core/plugin-info/PluginInfo.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class PluginInfo {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  version: string;
+
+  @Column()
+  author: string;
+
+  @Column({ type: 'text', nullable: true })
+  description?: string;
+
+  @Column({ default: false })
+  isEnabled: boolean;
+}
