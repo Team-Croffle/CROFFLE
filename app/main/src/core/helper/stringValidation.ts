8b8c56fc42fc1dd47bcee4c0ad1201fbaeed97a4
BlueNyang <dksrbxo@bluenyang.kr>
2025-12-29 23:47:26 +0900

    feat: add string validator

---
+++ app/main/src/core/helper/stringValidation.ts
@@ -0,0 +1,21 @@
+export const stringValidation = (
+  input: string,
+  nullable: boolean = false,
+  maxLength: number = 255,
+  minLength: number = 1,
+  pattern?: RegExp
+): boolean => {
+  if (!nullable && input === null) {
+    return false;
+  }
+  if (nullable && input === null) {
+    return true;
+  }
+  if (input.length < minLength || input.length > maxLength) {
+    return false;
+  }
+  if (pattern && !pattern.test(input)) {
+    return false;
+  }
+  return true;
+};
