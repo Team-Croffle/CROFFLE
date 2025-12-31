9cdea78ce69cafa15c3d3afd1e6ff99eaec29dc2
KimJiHun <kmr2397@naver.com>
2025-12-31 14:48:08 +0900

    feat: Api index 연결 및 데이터 처리 일부 수정

--- app/main/src/preload/index.ts
+++ app/main/src/preload/index.ts
@@ -3,12 +3,14 @@ import { windowApi } from './api/window.api';
 import { tagApi } from './api/tag.api';
 import { pluginInfoApi } from './api/pluginInfo.api';
 import { settingsApi } from './api/settings.api';
+import { scheduleApi } from './api/schedule.api';
 
 const electronAPI = {
   ...windowApi,
   ...tagApi,
   ...pluginInfoApi,
   ...settingsApi,
+  ...scheduleApi,
 };
 
 export type ElectronAPI = typeof electronAPI;
