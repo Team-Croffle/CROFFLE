c1e9c60ec2b655177a60c4336935d392f41a8f71
VenyVince <redguy0814@gmail.com>
2026-01-20 19:46:40 +0900

    refactor: Define Notification Exception Hnalding

--- app/main/src/core/native-os/service/OSService.ts
+++ app/main/src/core/native-os/service/OSService.ts
@@ -3,19 +3,23 @@ import { Notification, clipboard } from 'electron';
 export class OsService {
   // 1. 알림
   public async showNotification(title: string, body: string): Promise<void> {
-    console.log('알림', {title, body})  // test line
-    if (Notification.isSupported()) {
-      new Notification({ title, body }).show();
+  try {
+    if (!Notification.isSupported()) {
+      console.warn('OS 알림이 지원 X');
+      return; // 예외 내부 처리
     }
+
+    new Notification({ title, body }).show();
+  } 
+  catch (error) {
+    console.error('알림 실패 내용: ', error);
   }
+}
 
   // 2. 클립보드
   // 2-1) 읽기
   public async getClipboard(): Promise<string> {
-    const test  = clipboard.readText();
-    console.log(' main 클립보드 읽기 완료', test) // test line
-    return test;
-    // return clipboard.readText(); // real line
+    return clipboard.readText();
   }
 
   // 2-2) 쓰기
