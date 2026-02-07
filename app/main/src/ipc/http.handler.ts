f2b697f7f1c6c5ce5525c85a2ef09c344f532c80
KIM SUMIN <gms09065@naver.com>
2026-02-08 04:24:48 +0900

    refactor: unify http response handling and parsing

--- app/main/src/ipc/http.handler.ts
+++ app/main/src/ipc/http.handler.ts
@@ -1,26 +1,46 @@
-import { ipcMain } from "electron";
-import { Response } from "@croffledev/croffle-common";
+import { ipcMain } from 'electron';
+import { HttpResponse } from '@croffledev/croffle-common';
 import { httpService } from '../core/http/service/HttpService';
 
 export const registerHttpIpcHandlers = (): void => {
-    ipcMain.handle('http:get', async (_, url: string, params?: any, headers?: any): Promise<Response> => {
-        if (!url || typeof url !== 'string') {
-            throw new Error('[HTTP] Invalid URL.');
-        }
+  ipcMain.handle(
+    'http:get',
+    async (
+      _,
+      url: string,
+      params?: Record<string, string>,
+      headers?: Record<string, string>
+    ): Promise<HttpResponse> => {
+      if (!url || typeof url !== 'string') {
+        throw new Error('[HTTP] Invalid URL.');
+      }
 
-        return httpService.get(url, params, headers);
-    });
+      return httpService.get(url, params, headers);
+    }
+  );
 
-    ipcMain.handle('http:post', async(_, url:string, body?: unknown, headers?: any): Promise<Response> => {
-        if (!url || typeof url !== 'string') {
-            throw new Error('[HTTP] Invalid URL.');
-        }
+  ipcMain.handle(
+    'http:post',
+    async (
+      _,
+      url: string,
+      body?: unknown,
+      headers?: Record<string, string>
+    ): Promise<HttpResponse> => {
+      if (!url || typeof url !== 'string') {
+        throw new Error('[HTTP] Invalid URL.');
+      }
 
-        return httpService.post(url, body, headers);
-    });
+      return httpService.post(url, body, headers);
+    }
+  );
 };
 
 export interface HttpAPI {
-    get: (url: string, params?: any, headers?: any) => Promise<Response>;
-    post: (url: string, body?: unknown, headers?: any) => Promise<Response>;
+  get: (
+    url: string,
+    params?: Record<string, string>,
+    headers?: Record<string, string>
+  ) => Promise<HttpResponse>;
+  post: (url: string, body?: unknown, headers?: Record<string, string>) => Promise<HttpResponse>;
 }
