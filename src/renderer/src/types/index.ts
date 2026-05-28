77b89d2ba089285e1dd10f8b9c4b5b3cf65c3b68
BlueNyang <dksrbxo@bluenyang.kr>
2026-05-29 01:47:10 +0900

    feat: plugin for context menu

--- src/renderer/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-import type { FeatureContextMenu, FeatureView } from '@croffledev/croffle-types';
-
-export type PLuginFeatureView = FeatureView & { pluginId?: string };
-export type PluginFeatureContextMenu = FeatureContextMenu & { pluginId?: string };
